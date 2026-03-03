1f2012a321adbd005ff91c8118975e0e83da322f
george-poole <41405698+george-poole@users.noreply.github.com>
2026-03-03 15:38:53 +0000

    .

--- build.sh
+++ build.sh
@@ -1,9 +1,18 @@
-REMOTE=$1
+REMOTE=false
+DRY=false
+
+while [[ "$1" == --* ]]; do
+    case "$1" in
+        --remote)
+        REMOTE=true
+        shift
+        ;;
+    esac
+done
 WEB_DIR="website/"
 jupyter-book build $WEB_DIR
 
-if [ ${#REMOTE} -ne 0 ]
-then
+if $REMOTE; then
 ghp-import -n -p -f "$WEB_DIR/_build/html"
 fi
 
