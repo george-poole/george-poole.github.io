0bd4b968da608e253472776f18cc34ca8f9007b0
george-poole <george.r.poole@proton.me>
2026-04-16 18:50:38 +0100

    .

--- build.sh
+++ build.sh
@@ -12,8 +12,8 @@ done
 WEB_DIR="website/"
 jupyter-book build $WEB_DIR
 
+ln -sf "$WEB_DIR/_build/html/index.html" alias.html
+
 if $REMOTE; then
     ghp-import -n -p -f "$WEB_DIR/_build/html"
 fi
-
-ln -sf "$WEB_DIR/_build/html/index.html" alias.html
